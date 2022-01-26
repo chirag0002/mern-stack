9ec66e1d58021ee16c86458a79c8b85e5c2a9a4d
chirag0002 <varshneychirag34@gmail.com>
2022-01-26 14:21:18 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+dotenv.config({path:'./config.env'});
+require('./db/connection');
+const User = require('./model/userSchema');
+
+const PORT=process.env.PORT;
+
+const middleware = (req, res, next)=>{
+    console.log('middleware');
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello from the server');
+});
+
+app.get('/about', middleware, (req, res) => {
+    res.send('About from the server');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Contact from the server');
+});
+
+app.get('/signin', (req, res) => {
+    res.send('Log In from the server');
+});
+
+app.get('/signup', (req, res) => {
+    res.send('Register from the server');
+});
+
+app.listen(PORT, ()=> {
+    console.log(`server is running at port ${PORT}`);
+})
